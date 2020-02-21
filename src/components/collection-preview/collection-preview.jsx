e7c90c171cd0196dc934711ae508c16ec433bd01
suzauddaula <suzauddaula103@gmail.com>
2020-02-22 03:11:15 +0600

    cart add

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -7,14 +7,13 @@ import './collection-preview.styles.scss';
 const CollectionPreview=({title, items})=>{
     return <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
+        <div className='preview'>  
              {
                  items
                  .filter((item,idx)=>idx<4)
-                 .map(({id, ...otherItemProps})=>(
-                     <CollectionItem key={ id }{...otherItemProps}/>
-                 ))
-             }
+                 .map(item=>(
+                     <CollectionItem key={ item.id } item={item}/>
+                ))}
         </div>
     </div>
 };
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,22 @@
-import CartAtionTypes from './cart.types';
+import CartActionTypes from './cart.types';
+
 
 const INITIAL_STATE={
-    hidden:true
+    hidden:true,
+    cartItems:[]
 };
-
+ 
 const cartReducer=(state=INITIAL_STATE,action)=>{
     switch(action.type){
-        case CartAtionTypes.TOGGLE_CART_HIDDEN:
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden:!state.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
             }
         default:
             return state;
