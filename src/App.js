16cc4d86d42164bdc41108a4b8fb5e948d8c02c2
suzauddaula <suzauddaula103@gmail.com>
2020-02-25 00:32:56 +0600

    checkout increase and decrease item and qty

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 import HomePage from './pages/homepages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.comonent'
+import CheckoutPage from './pages/checkout/checkout.component'
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems ,selectCartTotal} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
-import CartItem from '../../components/cart-item/cart-item.component';
 
 const CheckoutPage=({cartItems,total})=>(
     <div className='checkout-page'>
@@ -26,10 +26,10 @@ const CheckoutPage=({cartItems,total})=>(
             </div>
         </div>
         {
-            cartItems.map(cartItem=>
-                CartItem.name
-            )
-        }
+        cartItems.map(cartItem=>
+        (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
